a6390fc1164e5effa1d98f8afaa0ff9152840bd5
Rafael Terán <rafaelterantorralbo@gmail.com>
2023-10-03 10:38:51 +0200

    Memory pairs (#5)
    * feat: memory pairs game
    
    * feat: remove animations
    
    * fix: improving a bit the performance
    
    * fix: improving some edge cases
    
    * fix: rename script
    
    * feat: add readme
    
    * chore: update README.md
    
    ---------
    
    Co-authored-by: Román Jiménez <drrouman@gmail.com>

---
+++ examples/vanilla/pairs/README.md
@@ -0,0 +1,1 @@
+In this genially we can view an example of this script: https://view.genial.ly/651695b80205340011647f03
---
+++ examples/vanilla/pairs/main.js
@@ -0,0 +1,155 @@
+const showTime = 500;
+
+const getRandomItemFromList = (list) =>
+  list[Math.floor(Math.random() * list.length)];
+
+const getRestItems = (items, excludeIds) =>
+  items.filter((item) => !excludeIds.includes(item.id));
+
+const getRandomizedPairs = (allItems) => {
+  const randomPairs = new Map();
+  const excludedIds = [];
+
+  allItems.forEach((item) => {
+    const randomItem = getRandomItemFromList(
+      getRestItems(allItems, [...excludedIds, item.id])
+    );
+
+    if (randomItem) {
+      randomPairs.set(item.id, randomItem);
+      excludedIds.push(randomItem.id);
+    }
+  });
+
+  return randomPairs;
+};
+
+export default function myPlugin(script, geniallyEngine) {
+  const { pairs, overlay, randomize, allIsOk } = script.config;
+
+  let lastIdClicked = undefined;
+  let animating = false;
+  const successPairs = [];
+  const pairsRelations = new Map();
+  const allItems = pairs.flatMap(({ item1Pair, item2Pair }) => [
+    item1Pair,
+    item2Pair,
+  ]);
+  const randomizedPairs = randomize
+    ? getRandomizedPairs(
+        allItems.map((v) => ({
+          ...v,
+          source: v.source,
+        }))
+      )
+    : undefined;
+
+  const resetItem = (item) => {
+    lastIdClicked = undefined;
+
+    geniallyEngine.setTimeout(() => {
+      item.source = overlay.source;
+    }, showTime);
+  };
+
+  const showItem = (item) => {
+    item.source = item.itemSource;
+  };
+
+  const selectItem = (item) => {
+    showItem(item);
+    lastIdClicked = item.referenceId;
+  };
+
+  const failPair = (item) => {
+    const lastItemClicked = allItems.find(
+      (item) => item.referenceId === lastIdClicked
+    );
+
+    [item, lastItemClicked].forEach((item) => {
+      showItem(item);
+      resetItem(item);
+    });
+  };
+
+  const successPair = (item) => {
+    if (animating) {
+      return;
+    }
+
+    const pairItem = allItems.find(
+      (i) => i.referenceId === pairsRelations.get(item.referenceId)
+    );
+
+    const items = [item, pairItem];
+
+    lastIdClicked = undefined;
+    successPairs.push(items);
+
+    items.forEach((item) => {
+      showItem(item);
+    });
+
+    animating = true;
+
+    geniallyEngine.setTimeout(() => {
+      items.forEach((item) => {
+        item.shown = false;
+      });
+      animating = false;
+    }, showTime);
+  };
+
+  const checkPair = (currentItem) => {
+    if (!currentItem.shown) {
+      return;
+    }
+
+    if (!lastIdClicked) {
+      selectItem(currentItem);
+    } else if (lastIdClicked === currentItem.referenceId) {
+      resetItem(currentItem);
+    } else if (lastIdClicked === pairsRelations.get(currentItem.referenceId)) {
+      successPair(currentItem);
+    } else {
+      failPair(currentItem);
+    }
+
+    if (successPairs.length === pairs.length && allIsOk.action) {
+      geniallyEngine.setTimeout(() => {
+        geniallyEngine.runAction(allIsOk.action);
+      }, showTime);
+    }
+  };
+
+  const init = (allItems) => {
+    overlay.shown = false;
+
+    allItems.forEach((item) => {
+      item.itemSource = randomize
+        ? randomizedPairs?.get(item.id)?.source
+        : item.source;
+      item.referenceId = randomize
+        ? randomizedPairs?.get(item.id)?.id
+        : item.id;
+      item.source = overlay.source;
+    });
+  };
+
+  script.slide.on("entered", () => {
+    init(allItems);
+
+    pairs.forEach(({ item1Pair, item2Pair }) => {
+      pairsRelations.set(item1Pair.id, item2Pair.id);
+      pairsRelations.set(item2Pair.id, item1Pair.id);
+
+      item1Pair.on("click", () => {
+        checkPair(item1Pair);
+      });
+
+      item2Pair.on("click", () => {
+        checkPair(item2Pair);
+      });
+    });
+  });
+}
